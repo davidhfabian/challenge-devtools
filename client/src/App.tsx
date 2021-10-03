d35b6fe0954ddc5be93b5d70220f018c2714363d
David Fabian <davidfabianok@gmail.com>
2021-10-03 00:09:54 -0300

    feat: start client project

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div className='App'>
+      <h1>Mercado Libre - Challenge</h1>
+    </div>
+  )
+}
+
+export default App
